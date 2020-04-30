5adf58eac5f2b0eb78b52e3999c03948597377a3
victorvos <me@victorvos.com>
2020-04-30 16:59:01 +0200

    two buttons one increments with 1 the other with 2

--- testjs/index.js
+++ testjs/index.js
@@ -1,3 +1,10 @@
-console.log("hallo");
 
+let count2 = 0;
+let $thisTwo = document.querySelector("#this-two");
+
+function addTwo(){
+    count2+=2;
+    console.log("count2",count2);
+    $thisTwo.innerHTML = count2;
+}
 
