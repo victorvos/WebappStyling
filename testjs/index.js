7b395d7b5a59ba0d04f160d860044895fbf9262f
victorvos <me@victorvos.com>
2020-05-01 20:09:30 +0200

    tweede button werkt nu ook, weliswaar in html file allebei.

--- testjs/index.js
+++ testjs/index.js
@@ -3,8 +3,8 @@ let count2 = 0;
 let $thisTwo = document.querySelector("#this-two");
 
 function addTwo(){
-    count2+=2;
-    console.log("count2",count2);
-    $thisTwo.innerHTML = count2;
+    console.log(count);
+    count+=2;
+    $thisTwo.innerHTML = count;
 }
 
