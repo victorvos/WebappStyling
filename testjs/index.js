1cb2a3da4bd64bca3a33190c25988b1daa0811a0
victorvos <me@victorvos.com>
2020-05-02 15:13:54 +0200

    arrow function met parameter werkt nu ook

--- testjs/index.js
+++ testjs/index.js
@@ -1,10 +1,23 @@
-
-let count2 = 0;
-let $thisTwo = document.querySelector("#this-two");
-
-function addTwo(){
+let addOne = () => {
     console.log(count);
-    count+=2;
-    $thisTwo.innerHTML = count;
+    count++;
+    document.getElementById("this-one").innerHTML = count;
 }
+
+
+
+
+let count = 0;
+let count2 = 0;
+let two = 2; // parameter
+let $thisTwo = document.querySelector("#this-two");
+document.getElementById("button1").addEventListener('click', addOne);      
+document.getElementById("button2").addEventListener('click', () =>{
+    console.log(count);
+    count+=two;
+    $thisTwo.innerHTML = count;
+}); 
+
+
+
 
