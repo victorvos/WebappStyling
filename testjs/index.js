f30c48a19d772cb019f6d83d06d75b2caed1fcfc
victorvos <me@victorvos.com>
2020-05-02 16:00:57 +0200

    het doorgeven van de input field naar JS variabele werkt nog niet. Rest wel.

--- testjs/index.js
+++ testjs/index.js
@@ -4,20 +4,26 @@ let addOne = () => {
     document.getElementById("this-one").innerHTML = count;
 }
 
-
-
-
 let count = 0;
 let count2 = 0;
+let count3 = 0;
 let two = 2; // parameter
+
 let $thisTwo = document.querySelector("#this-two");
+let $thisThree = document.querySelector('#this-three');
+
 document.getElementById("button1").addEventListener('click', addOne);      
 document.getElementById("button2").addEventListener('click', () =>{
-    console.log(count);
-    count+=two;
-    $thisTwo.innerHTML = count;
-}); 
+    console.log(count2);
+    count2+=two;
+    $thisTwo.innerHTML = count2;
+});
 
+let $addingAmount = document.querySelector('#adding-amount').value;
+$addingAmount = Number($addingAmount);
 
-
-
+document.getElementById("buttonAddInputAmount").addEventListener('click', () =>{
+    console.log(count3);
+    count3+=$addingAmount;
+    $thisThree.innerHTML = count3;
+})
